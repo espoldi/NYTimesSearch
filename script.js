836530c085d6b8bc24a17cb80bf314a98eba1e7e
sossw1 <sossw1@gmail.com>
2020-12-03 21:17:32 -0500

    function to get search params

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var apiKEY = "tTqVF08iHcdtycoBQ5PvpXVTNGMxMPJH";
+
 var search = "Biden";
 
 var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apiKEY}`;
+
+function getSearchParams() {
+    let val = $("#searchTerm")[0].value;
+    console.log(val);
+}
+
 
 $.ajax({
     url: queryURL,
